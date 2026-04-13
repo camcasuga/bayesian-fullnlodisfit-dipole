2c7c046888be9507c7a32e7dedd6f8aaacf069cb
camcasuga <carlislemarquez.casuga@gmail.com>
2026-04-13 11:33:52 +0300

    Revise README, add all posterior samples and BK datafiles, add c++ and python code to read files, add unit tests

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# bayesian-fullnlodisfit-dipole
-Posterior samples and corresponding BK dipole amplitudes from Bayesian fit at NLO to DIS data, arXiv:2604.XXXXX
+# NLO DIS fit
+This repository contains Python and C++ codes to evaluate dipole amplitudes using grids from the Bayesian fit to HERA total inclusive and charm production data of the BK initial condition at full NLO+NLL accuracy published in arXiv:2604.XXXXX
+
+## Python
+The python code can be found from the python folder. It requires numpy, scipy, and matplotlib packages.
+
+### Usage
+The necessary code to read in the BK evolved dipole amplitudes and construct an interpolator (in both dipole size $r$ and evolutoin rapidity $Y$) can be found from `python/nlodipole.py`. Typical usecase is demonstrated below:
+```python
+import nlodipole
+
+bk_file = "/path/to/bk_datafile.dat"
+
+dipole = nlodipole.BKDipole(bk_file)
+
+# Evaluate the dipole
+print(f"N(r=0.2GeV^-1, Y=1) = {dipole.N(Y=1,r=0.2)}")
+```
+Additional examples can be found from `python/read_bk.py` and `python/read_bk_allparams.py`
+
+The code `python/read_bk.py` prints and plots the dipole amplitude with the BK datafile and rapidity value as input. Example usage for Y = 0:
+```bash
+python python/read_bk.py data/mv,balsd/median_bk.dat 0
+```
+which plots the dipole amplitude N as function of r given Y = 0.
+
+Meanwhile, `python/read_bk_allparams.py` loads all BK datafiles in a folder and plots the mean and 2-sigma band N(r) given rapidity Y. Example usage for Y = 0:
+```bash
+python python/read_bk_allparams.py data/mv,balsd/bks 0
+```
+
+#### Python Unit Tests
+
+To run the Python unit tests, run:
+```bash
+python python/tests.py
+```
+
+
+## C++
+The C++ code can be found from the cpp folder. It requires GSL, and CMake is used to compile the code.
+
+### Compile
+To compile, in the `cpp` folder run
+```bash
+mkdir build
+cd build
+cmake ..
+make
+```
+
+
+### Usage
+See `cpp/src/amplitudelib.hpp` for details. For examples, see `cpp/src/dipole_amplitude.cpp` and `cpp/src/tests/tests.cpp`
+
+Typical workflow as an example:
+```c++
+const std::string datafile="../data/mv,balsd/median_bk.dat";
+// Read data
+AmplitudeLib dipole(datafile);
+
+// Evaluate dipole at r=0.4 GeV^-1 at evolution rapidity Y=0.3
+double Y=0.3;
+cout << "N(r=0.4 GeV^-1, Y=" << Y <<") = " << dipole.DipoleAmplitude(0.4, Y) << endl;
+
+// If you are evaluating the dipole many times at the same evolution rapidity, 
+// to speed up the evaluation one can initialize the interpolation first
+dipole.InitializeInterpolation(Y);
+```
+
+
+#### C++ unit tests 
+In order to check that the setup works without problems, run unit tests by running in the `cpp` folder the command
+```bash
+./build/bin/test
+```
+All tests should pass. The tests cover internal code dynamics and also reproduce some numbers reported in the publication.
+
+There is also an example code `cpp/src/dipole_amplitude.cpp` that is compiled to produce the binary `build/bin/dipole`. That code takes as an input the BK datafile:
+```bash
+./build/bin/dipole path/to/bk/datafile
+```
+
+## License
+
+[![CC BY 4.0][cc-by-shield]][cc-by]
+
+This work is licensed under a
+[Creative Commons Attribution 4.0 International License][cc-by].
+
+
+[cc-by]: https://creativecommons.org/licenses/by/4.0/
+[cc-by-shield]: https://licensebuttons.net/l/by/4.0/80x15.png
---
+++ python/tests.py
@@ -0,0 +1,103 @@
+# Unit tests that test and demonstrate the functionality of the Python module
+import unittest
+import nlodipole 
+import numpy as np
+
+class TestNlodipole(unittest.TestCase):
+    def test_GBW(self):
+        # file data/gbw.dat corresponds to the GBW dipole amplitude
+        # N(r,Y) = 1 - exp(-r^2 Q_s^2(Y)/4)
+        # with Q_s^2(Y) = 1.0*exp(lambda*Y) [GeV^2] and lambda=1/3
+
+        bk_file_dir = 'data/gbw.dat'  
+        Y = 0.5
+        gbwinterpolator = nlodipole.BKDipole(bk_file_dir)
+
+        r_values = [1e-2, 1e-1, 1, 10]  # Example r values
+        N_values = [gbwinterpolator.N(Y, r) for r in r_values]
+        Qs2 = 1.0 * np.exp(1/3 * Y)  # Q_s^2(Y)
+        true_values = [1. - np.exp(-r**2 * Qs2 / 4) for r in r_values]
+        
+        for computed, true in zip(N_values, true_values):
+            self.assertAlmostEqual(computed, true, places=3)
+
+        # Test evaluating the dipole at fixed Y and r
+        r=0.2
+        Y=2.6
+        model=gbwinterpolator.N(Y=Y, r=r)
+        Qs2 = 1.0 * np.exp(1/3 * Y) 
+        true = 1.0 - np.exp(-r**2 * Qs2 / 4)
+        self.assertAlmostEqual(model, true, places=3)
+
+    def test_bal_sd_qs(self):
+
+        # Test for MV initial condition
+        bk_file_dir = "data/mv,balsd/median_bk.dat"
+        Y=np.log(1/0.01)
+        interpolator = nlodipole.BKDipole(bk_file_dir)
+
+        # N(r^2=2/Q_s^2, Y) = 1 - exp(-0.5)
+        # Check Q_s^2 quoted in Table 1
+        true_qs2=0.039
+        dipval = interpolator.N(Y, np.sqrt(2/true_qs2))
+        self.assertAlmostEqual(dipval, 1 - np.exp(-0.5), places=2)
+
+        # Test for MV gamma initial condtion
+        bk_file_median = "data/mvgam,balsd/median_bk.dat"
+        interpolator_median = nlodipole.BKDipole(bk_file_median)
+        true_qs2_median = 0.185
+        dipval=interpolator_median.N(Y, np.sqrt(2/true_qs2_median))
+        self.assertAlmostEqual(dipval, 1 - np.exp(-0.5), places=2)
+
+    def test_interpolation_boundaries(self):
+        bk_file_dir = "data/mv,balsd/median_bk.dat"
+        interpolator = nlodipole.BKDipole(bk_file_dir)
+
+         # Check that we get NaN outside the range of the interpolator
+        self.assertTrue(np.isnan( interpolator.N(-1, 2) )) # negative Y = before initial condition
+        self.assertTrue(np.isnan( interpolator.N(9999, 2) )) # too large Y
+
+        self.assertAlmostEqual(interpolator.N(1, 99999), 1, places=5) # large r, should return 1
+        self.assertAlmostEqual(interpolator.N(1, 1e-30), 0, places=5) # small r, should return 1
+
+        # r=0
+        self.assertAlmostEqual(interpolator.N(r=0, Y=1), 0, places=5)
+
+        # MVgamma, balsd test
+        bk_file_dir = "data/mvgam,balsd/median_bk.dat"
+        interpolator = nlodipole.BKDipole(bk_file_dir)
+        self.assertTrue(np.isnan( interpolator.N(-1, 2) )) # negative Y = before initial condition
+        self.assertTrue(np.isnan( interpolator.N(9999, 2) )) # too large Y
+        self.assertAlmostEqual(interpolator.N(1, 99999), 1, places=5) # large r, should return 1
+        self.assertAlmostEqual(interpolator.N(1, 1e-30), 0, places=5) # small r, should return 1
+        self.assertAlmostEqual(interpolator.N(r=0, Y=1), 0, places=5)
+
+        # MVgamma, parent dipole test
+        bk_file_dir = "data/mvgam,pd/median_bk.dat"
+        interpolator = nlodipole.BKDipole(bk_file_dir)
+
+         # Check that we get NaN outside the range of the interpolator
+        self.assertTrue(np.isnan( interpolator.N(-1, 2) )) # negative Y = before initial condition
+        self.assertTrue(np.isnan( interpolator.N(9999, 2) )) # too large Y
+
+        self.assertAlmostEqual(interpolator.N(1, 99999), 1, places=5) # large r, should return 1
+        self.assertAlmostEqual(interpolator.N(1, 1e-30), 0, places=5) # small r, should return 1
+
+        # r=0
+        self.assertAlmostEqual(interpolator.N(r=0, Y=1), 0, places=5)
+
+
+
+    def test_parent_qs(self):
+        # Test the parent Q_s^2 for the Balitsky-Satya parent dipole
+        bk_file_dir = "data/mvgam,pd/median_bk.dat"
+        Y = np.log(1/0.01)
+        interpolator = nlodipole.BKDipole(bk_file_dir)
+
+        # Check Q_s^2 quoted in Table 1
+        true_qs2 = 0.208
+        dipval = interpolator.N(Y, np.sqrt(2/true_qs2))
+        self.assertAlmostEqual(dipval, 1 - np.exp(-0.5), places=2)
+
+if __name__ == '__main__':
+    unittest.main()
